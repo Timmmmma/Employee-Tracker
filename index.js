b9c38f44070e86f8759539981511886ab9cac839
zhentian <zhentian222@gmail.com>
2022-07-01 03:43:14 -0700

    part of comment update

--- index.js
+++ index.js
@@ -41,11 +41,9 @@ connection.connect(err => {
   });
 });
 
-// Inital Prompt - Main Menu
+//Main page
 const mainPage = () => {
     inquirer
-
-        // Prompt the user
         .prompt({
             type: 'list',
             name: 'chioces',
@@ -63,29 +61,29 @@ const mainPage = () => {
                 'I am finished'
             ]
         })
-        // Take the data and use switch statements to decide what to do per option
+        //Go to the next function according to users choice
         .then((data) => {
             switch (data['chioces']) {
                 case 'View All Employees':
-                    viewAllEmp();
+                    viewAllEmployees();
                     break;
                 case 'View All Employees By Department':
-                    viewEmpByDep();
+                    viewEmployeeByDepartment();
                     break;
                 case 'View All Employees By Manager':
-                    viewEmpByMngt();
+                    viewEmployeeByManagment();
                     break;
                 case 'Add Employee':
-                    addEmp();
+                    addEmployee();
                     break;
                 case 'Update Employee Role':
-                    upEmp();
+                    updateEmployee();
                     break;
                 case 'View All Department':
-                    viewDep();
+                    viewDepartment();
                     break;
                 case 'Add Department':
-                    addDep();
+                    addDepartment();
                     break;
                 case 'View Roles':
                     viewRoles();
@@ -102,11 +100,9 @@ const mainPage = () => {
 };
 
 //-------------------------------Employee---------------------------------
-const viewAllEmp = () => {
-
-    // connect to db and asking the following query
+//View Employees/ READ all, SELECT * FROM
+const viewAllEmployees = () => {
     connection.query(
-        // Manipulate tables to view all employees
         `SELECT employee.id, employee.first_name, employee.last_name, roles.title AS role, roles.salary AS salary, manager.first_name AS manager,
         department.name AS department 
         FROM employee
@@ -117,28 +113,24 @@ const viewAllEmp = () => {
         LEFT JOIN manager
         ON employee.manager_id = manager.id`,
 
-        // Call back function to decide what to do with data
         function (err, results) {
             if (err) {
                 console.log(err.message);
                 return;
             }
 
-            // Show the results as a table to the user
+            //Show the table
             console.table(results);
 
-            // Re-prompt the user
+            //Return to the main page
             mainPage();
         }
     );
 };
 
-// View Employees by Department
-const viewEmpByDep = () => {
-
-    // Connect to db
+//"View Employees by Department" / READ by, SELECT * FROM
+const viewEmployeeByDepartment = () => {
     connection.query(
-        // Get the table contents from department table
         `SELECT * FROM department`,
 
         function (err, results, fields) {
@@ -146,22 +138,19 @@ const viewEmpByDep = () => {
                 console.log(err.message);
                 return;
             }
-            // Create empty array for storing info
-            depArr = [];
-            // for each item in the results array, push the name of the department to the department array
+            //Make an array for departments and select from here so it will not miss any newly added department
+            departmentArray = [];
             results.forEach(item => {
-                depArr.push(item.name)
+                departmentArray.push(item.name)
             });
             inquirer
                 .prompt({
                     type: 'list',
                     name: 'filter-emp-dep',
-                    message: 'Choose a department to filter from:',
-                    // Choices are from the department array, this will allow for the user to add departments in future
-                    choices: depArr
+                    message: 'Choose a department from below:',
+                    choices: departmentArray
                 })
                 .then((data) => {
-                    // Take the data and filter based on what user chose
                     connection.query(
                         `SELECT employee.id, employee.first_name, employee.last_name, department.name AS department 
                             FROM employee
@@ -170,17 +159,14 @@ const viewEmpByDep = () => {
                             LEFT JOIN department
                             ON roles.department_id = department.id
                             WHERE department.name = ?`,
-                        // Value user chose that will be replaced with question mark, this prevents SQL Injection attacks
                         [data['filter-emp-dep']],
                         function (err, results, fields) {
                             if (err) {
                                 console.log(err.message);
                                 return;
                             }
-
-                            // Show results as table
                             console.table(results);
-                            // Reprompt user
+                            //Return to main page
                             mainPage();
                         }
                     )
@@ -189,21 +175,19 @@ const viewEmpByDep = () => {
     );
 };
 
-//  View Employees by Managment
-const viewEmpByMngt = () => {
+//"View Employees by Department" / READ by, SELECT * FROM
+const viewEmployeeByManagment = () => {
     connection.query(
-        // Get the table contents from manager table
         `SELECT * FROM manager`,
         function (err, results, fields) {
             if (err) {
                 console.log(err.message);
                 return;
             }
-            // Create empty array for storing info
-            manArr = [];
-            // for each item in the results array, push the name of the manager to the manager array
+            //Make an array for managers and select from here so it will not miss any newly added manager
+            managerArray = [];
             results.forEach(item => {
-                manArr.push(item.first_name)
+                managerArray.push(item.first_name)
             })
 
             inquirer
@@ -211,8 +195,7 @@ const viewEmpByMngt = () => {
                     type: 'list',
                     name: 'filter-emp-man',
                     message: 'Choose a manager to filter from:',
-                    // Choices are from the manager array, this will allow for the user to add departments in future
-                    choices: manArr
+                    choices: managerArray
                 })
                 .then((data) => {
                     connection.query(
@@ -221,17 +204,14 @@ const viewEmpByMngt = () => {
                             LEFT JOIN manager
                             ON employee.manager_id = manager.id
                             WHERE manager.first_name = ?`,
-                        // Value user chose that will be replaced with question mark, this prevents SQL Injection attacks
                         [data['filter-emp-man']],
                         function (err, results, fields) {
                             if (err) {
                                 console.log(err.message);
                                 return;
                             }
-
-                            // Show results as table
                             console.table(results);
-                            // Reprompt user
+                            //Return to main page
                             mainPage();
                         }
                     );
@@ -242,11 +222,9 @@ const viewEmpByMngt = () => {
 };
 
 // Add a new employee
-const addEmp = () => {
-
-    // Connect to DB
+const addEmployee = () => {
     connection.query(
-        // Select all roles from table for future ref
+        //Selcet the role table first
         `SELECT * FROM roles`,
         function (err, results, fields) {
             if (err) {
@@ -254,16 +232,13 @@ const addEmp = () => {
                 return;
             }
 
-            // Create empty array for storing info
-            let roleArr = [];
-
-            // for each item in the results array, push the name of the roles to the roles array
+            ////Make an array for role and select from here so it will not miss any newly added role
+            let roleArray = [];
             results.forEach(item => {
-                roleArr.push(item.title)
+                roleArray.push(item.title)
             })
-            // Connect to db again 
             connection.query(
-                // Select all managers from managers table for future ref
+                //Selcet the manager tbale second
                 `SELECT * FROM manager`,
                 function (err, results, fields) {
                     if (err) {
@@ -271,15 +246,11 @@ const addEmp = () => {
                         return;
                     }
 
-                    // Create empty array for managers
-                    let manArr = [];
-
-                    // For each item in results array, push the name of the manager to the manager array
+                    let managerArray = [];
                     results.forEach(item => {
-                        manArr.push(item.first_name)
+                        managerArray.push(item.first_name)
                     });
 
-                    // Prompt the user
                     inquirer
                         .prompt([
                             {
@@ -296,38 +267,28 @@ const addEmp = () => {
                                 type: 'list',
                                 name: 'role_pick',
                                 message: 'What will you employees role be?',
-                                // use the names from the roles array to get the roles, this will allow us to add new roles in the future
-                                choices: roleArr
-                            },                       
+                                choices: roleArray
+                            },
+                            {
+                                type: 'list',
+                                name: 'mngt_pick',
+                                message: 'Who will your employees manager be?',
+                                choices: managerArray
+                            }                       
                         ])
                         .then((data) => {
-                            // Create a loop of the role arr in order to compare the users answer to the position it is in in the array,
-                            // this will provide us with a number that can be used as an id for the role_id section of our table
                             let role_id;
-                            for (i = 0; i < roleArr.length; i++) {
-                                if (data.role_pick === roleArr[i]) {
+                            for (i = 0; i < roleArray.length; i++) {
+                                if (data.role_pick === roleArray[i]) {
                                     role_id = i + 1
                                 }
                             }
-
-                            // if statement that will decide weather or not based on users input if the employee is a manager or not 
-                            let manager_confirm;
-                            if (data.mngt_confirm === true) {
-                                manager_confirm = 1;
-                            } else {
-                                manager_confirm = 0
-                            }
-
                             let manager_id;
-
-                            // if the mngt_pick prompt was not run and returns nothing set the manager_id to null
                             if (!data.mngt_pick) {
                                 manager_id = null;
-                                // else Create a loop of the manager arr in order to compare the users answer to the position it is in in the array,
-                                // this will provide us with a number that can be used as an id for the manager_id section of our table
                             } else {
-                                for (i = 0; i < manArr.length; i++) {
-                                    if (data.mngt_pick === manArr[i]) {
+                                for (i = 0; i < managerArray.length; i++) {
+                                    if (data.mngt_pick === managerArray[i]) {
                                         manager_id = i + 1
                                     }
                                 }
@@ -335,9 +296,9 @@ const addEmp = () => {
                             // Connect to db again
                             connection.query(
                                 // Insert values from user into db, uses place holders to prevent SQL Injection attack
-                                `INSERT INTO employee (first_name, last_name, role_id, manager_id, manager_confirm)
-                                    VALUES (?, ?, ?, ?, ?)`,
-                                [data.first_name, data.last_name, role_id, manager_id, manager_confirm],
+                                `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                                    VALUES (?, ?, ?, ?)`,
+                                [data.first_name, data.last_name, role_id, manager_id],
                                 function (err, results, fields) {
                                     if (err) {
                                         console.log(err.message);
@@ -355,8 +316,8 @@ const addEmp = () => {
     );
 };
 
-// updating the employee 
-const upEmp = () => {
+//Select a employee then update his role
+const updateEmployee = () => {
     // Select all roles from table for future ref
     connection.query(
         `SELECT * FROM roles`,
@@ -367,11 +328,11 @@ const upEmp = () => {
             }
 
             // Create empty array for storing info
-            let roleArr = [];
+            let roleArray = [];
 
             // for each item in the results array, push the name of the roles to the roles array
             results.forEach(item => {
-                roleArr.push(item.title)
+                roleArray.push(item.title)
             })
             connection.query(
                 `SELECT first_name, last_name FROM employee`,
@@ -403,13 +364,13 @@ const upEmp = () => {
                                 type: 'list',
                                 name: 'role_select',
                                 message: 'Please select a role you would like your employee to change to:',
-                                choices: roleArr
+                                choices: roleArray
                             }
                         ])
                         .then((data) => {
                             let role_id;
-                            for (let i = 0; i < roleArr.length; i++) {
-                                if (data.role_select === roleArr[i]) {
+                            for (let i = 0; i < roleArray.length; i++) {
+                                if (data.role_select === roleArray[i]) {
                                     role_id = i + 1;
                                 }
                             };
@@ -440,7 +401,8 @@ const upEmp = () => {
 };
 
 //----------------------------Department---------------------------------
-const viewDep = () => {
+//"View all Department" / READ all, SELECT * FROM
+const viewDepartment = () => {
     connection.query(
         `SELECT * FROM department`,
         function (err, results, fields) {
@@ -455,8 +417,8 @@ const viewDep = () => {
     )
 } 
 
-// adding department 
-const addDep = () => {
+//Add a new department 
+const addDepartment = () => {
     inquirer
         .prompt({
             type: 'text',
@@ -482,6 +444,7 @@ const addDep = () => {
 }
 
 //-------------------------------Role-----------------------------------------
+//View all Roles/ READ all, SELECT * FROM
 const viewRoles = () => {
     connection.query(
         `SELECT roles.id, roles.title, roles.salary, department.name
@@ -500,7 +463,7 @@ const viewRoles = () => {
     );
 };
 
-// adding roles
+//Add a new roles
 const addRole = () => {
     connection.query(
         `SELECT * FROM department`,
@@ -510,9 +473,9 @@ const addRole = () => {
                 return;
             }
 
-            let depArr = [];
+            let departmentArray = [];
             results.forEach(item => {
-                depArr.push(item.name)
+                departmentArray.push(item.name)
             })
 
             inquirer
@@ -531,14 +494,14 @@ const addRole = () => {
                         type: 'list',
                         name: 'department',
                         message: 'Please select the department you role will be a part of: ',
-                        choices: depArr
+                        choices: departmentArray
                     }
                 ])
                 .then((data) => {
                     let department_id;
 
-                    for (let i = 0; i < depArr.length; i++) {
-                        if (depArr[i] === data.department) {
+                    for (let i = 0; i < departmentArray.length; i++) {
+                        if (departmentArray[i] === data.department) {
                             department_id = i + 1;
                         };
                     };
